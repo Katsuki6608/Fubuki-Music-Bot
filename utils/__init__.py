dafa076109b7f539332cfad6de0f3974067814a5
Katsuki6608 <katsuki6608@gmail.com>
2026-09-17 14:53:33 +0530

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,20 @@
+# Fubuki Music Bot Engine
+# Optimized for Termux / Snapdragon 680 Architecture
+
+from .channelplay import *
+from .database import *
+from .decorators import *
+from .formatters import *
+from .inline import *
+from .pastebin import *
+from .sys import *
+
+__all__ = [
+    "channelplay",
+    "database",
+    "decorators",
+    "formatters",
+    "inline",
+    "pastebin",
+    "sys",
+]
