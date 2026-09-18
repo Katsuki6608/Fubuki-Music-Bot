a45c6c405ff814439d00e34c21b6e6d6ec9f1274
Katsuki6608 <katsuki6608@gmail.com>
2026-09-18 14:26:26 +0530

    Add files via upload

---
+++ Fubuki/plugins/tools/ping.py
@@ -0,0 +1,39 @@
+# All rights reserved.
+#
+from datetime import datetime
+
+from pyrogram.types import Message
+
+from config import BANNED_USERS, PING_IMG_URL
+from strings import command
+from Fubuki import app
+from Fubuki.core.call import Fubuki_Call as Ayush
+from Fubuki.utils import bot_sys_stats
+from Fubuki.utils.decorators.language import language
+from Fubuki.utils.inline import support_group_markup
+
+
+@app.on_message(command("PING_COMMAND") & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+        has_spoiler=True,
+    )
+    start = datetime.now()
+    pytgping = await Ayush.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+        ),
+        reply_markup=support_group_markup(_),
+    )
