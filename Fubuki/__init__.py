312f8b570702052ebf9bf9f3c9b745e1a080845a
Katsuki6608 <katsuki6608@gmail.com>
2026-09-18 15:31:36 +0530

    Update __init__.py

--- Fubuki/__init__.py
+++ Fubuki/__init__.py
@@ -1,11 +1,10 @@
-#
 # All rights reserved.
 
 from Fubuki.core.bot import AyuBot
 from Fubuki.core.dir import dirr
 from Fubuki.core.git import git
 from Fubuki.core.userbot import Userbot
-from Fubuki.misc import dbb, heroku, sudo
+from Fubuki.misc import dbb, sudo
 
 from .logging import LOGGER
 
@@ -17,9 +16,6 @@ git()
 
 # Initialize Memory DB
 dbb()
-
-# Heroku APP
-heroku()
 
 # Load Sudo Users from DB
 sudo()
