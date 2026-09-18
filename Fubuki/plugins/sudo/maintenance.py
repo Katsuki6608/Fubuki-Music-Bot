677c521bc48d0655f2c9f1687840ae81acc5ee3b
Katsuki6608 <katsuki6608@gmail.com>
2026-09-18 16:40:36 +0530

    Update maintenance.py

--- Fubuki/plugins/sudo/maintenance.py
+++ Fubuki/plugins/sudo/maintenance.py
@@ -3,7 +3,7 @@
 
 from pyrogram.types import Message
 
-from strings import get_string, command
+from strings import command, get_string
 from Fubuki import app
 from Fubuki.misc import SUDOERS
 from Fubuki.utils.database import (
@@ -21,23 +21,25 @@ async def maintenance(client, message: Message):
         _ = get_string(language)
     except Exception:
         _ = get_string("en")
+
     usage = _["maint_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    message.chat.id
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
+
+    state = message.text.split(None, 1)[1].strip().lower()
+    currently_on = bool(await is_maintenance())
+
     if state == "enable":
-        if await is_maintenance() is False:
+        if currently_on:
             await message.reply_text(_["maint_6"])
         else:
             await maintenance_on()
             await message.reply_text(_["maint_2"])
     elif state == "disable":
-        if await is_maintenance() is False:
+        if not currently_on:
+            await message.reply_text(_["maint_5"])
+        else:
             await maintenance_off()
             await message.reply_text(_["maint_3"])
-        else:
-            await message.reply_text(_["maint_5"])
     else:
         await message.reply_text(usage)
