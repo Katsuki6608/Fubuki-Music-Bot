a87b4db45884374ab08adc3bd03e0b350b6927e4
Katsuki6608 <katsuki6608@gmail.com>
2026-09-18 16:46:00 +0530

    Update autoend.py

--- Fubuki/plugins/sudo/autoend.py
+++ Fubuki/plugins/sudo/autoend.py
@@ -1,26 +1,35 @@
 # All rights reserved.
 #
 
-from strings import command
+from pyrogram.types import Message
+
+from strings import command, get_string
 from Fubuki import app
 from Fubuki.misc import SUDOERS
-from Fubuki.utils.database import autoend_off, autoend_on
+from Fubuki.utils.database import autoend_off, autoend_on, get_lang, is_autoend
 
 
 @app.on_message(command("AUTOEND_COMMAND") & SUDOERS)
-async def auto_end_stream(client, message):
+async def auto_end_stream(client, message: Message):
+    try:
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+    except Exception:
+        _ = get_string("en")
+
     usage = "**ᴜsᴀɢᴇ:**\n\n/autoend [enable|disable]"
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
+
+    state = message.text.split(None, 1)[1].strip().lower()
+
     if state == "enable":
         await autoend_on()
         await message.reply_text(
-            "Auto End enabled.\n\nBot will leave voicechat automatically after 30 secinds if one is listening song with a warning message.."
+            "Auto End Stream Enabled.\n\nBot will automatically leave the voice chat after 3 minutes if nobody is listening."
         )
     elif state == "disable":
         await autoend_off()
-        await message.reply_text("Autoend disabled")
+        await message.reply_text("Auto End Stream Disabled.")
     else:
         await message.reply_text(usage)
