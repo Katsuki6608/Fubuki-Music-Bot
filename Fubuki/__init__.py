0be679a534f14951ad509df05a1cdcbf782c1208
Katsuki6608 <katsuki6608@gmail.com>
2026-09-18 15:24:24 +0530

    Create __init__.py

---
+++ Fubuki/__init__.py
@@ -0,0 +1,36 @@
+#
+# All rights reserved.
+
+from Fubuki.core.bot import AyuBot
+from Fubuki.core.dir import dirr
+from Fubuki.core.git import git
+from Fubuki.core.userbot import Userbot
+from Fubuki.misc import dbb, heroku, sudo
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AyuBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import PlaTForms
+
+Platform = PlaTForms()
+HELPABLE = {}
