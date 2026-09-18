ba1ae07e6260d0f3c637708b3ef3c4af8bb7fbbe
Katsuki6608 <katsuki6608@gmail.com>
2026-09-18 17:05:31 +0530

    Update videomode.py

--- Fubuki/plugins/sudo/videomode.py
+++ Fubuki/plugins/sudo/videomode.py
@@ -7,7 +7,7 @@ import config
 from strings import command
 from Fubuki import app
 from Fubuki.misc import SUDOERS
-from Fubuki.utils.database import add_off, add_on
+from Fubuki.utils.database import add_off, add_on, is_on_off
 from Fubuki.utils.decorators.language import language
 
 
@@ -17,13 +17,19 @@ async def videoloaymode(client, message: Message, _):
     usage = _["vidmode_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
+
+    state = message.text.split(None, 1)[1].strip().lower()
+    target_var = getattr(config, "YTDOWNLOADER", 1)
+
     if state == "download":
-        await add_on(config.YTDOWNLOADER)
+        if await is_on_off(target_var):
+            return await message.reply_text(_["vidmode_2"])
+        await add_on(target_var)
         await message.reply_text(_["vidmode_2"])
     elif state == "m3u8":
-        await add_off(config.YTDOWNLOADER)
+        if not await is_on_off(target_var):
+            return await message.reply_text(_["vidmode_3"])
+        await add_off(target_var)
         await message.reply_text(_["vidmode_3"])
     else:
         await message.reply_text(usage)
